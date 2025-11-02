6fea1911753dfdd0b7755b41b7a1f6f8582c651b
lsbjordao <tucarj@gmail.com>
2025-11-02 19:59:18 -0300

    add robsonii fig

--- taxon/Mimosa/Mimosa_robsonii.ts
+++ taxon/Mimosa/Mimosa_robsonii.ts
@@ -210,5 +210,19 @@ source2.figure = '6'
 source2.obtainingMethod = 'drawing'
 Mimosa_robsonii.addSource(source2)
 
+const source3 = new Source()
+source3.obraPrinceps = 'yes'
+source3.sourceType = 'article'
+source3.authorship = 'Jordão, L.S.B. and Morim, M.P. and Simon, M.F., Dutra, V.F. and Baumgratz, J.F.A.'
+source3.year = '2021'
+source3.title = 'New Species of *Mimosa* (Leguminosae) from Brazil'
+source3.journal = 'Systematic Botany'
+source3.volume = '46'
+source3.number = '7A'
+source3.pages = '347'
+source3.imageUrl = 'https://github.com/lsbjordao/TTS-Mimosa-App-Figures/blob/main/Mimosa/Mimosa_robsonii/Mimosa_robsonii_fruit_0.png?raw=true'
+source3.imageUrlLegend = 'Detail of craspedia. A. *M. robsonii*. (Photo A. Collection *R.D. Ribeiro 1044*).'
+Mimosa_robsonii.fruit.addSource(source1)
+
 // Export Mimosa robsonii
 export { Mimosa_robsonii }
